1602fa48a7ac421854ed76072dd7f28348420175
Tadeáš Vintrlík <xvintr04@stud.fit.vutbr.cz>
2021-07-09 10:43:33 +0200

    add few subtree filter tests

--- tests/test_filter.c
+++ tests/test_filter.c
@@ -42,6 +42,7 @@ local_setup(void **state)
     sr_conn_ctx_t *conn;
     const char *features[] = {NULL};
     const char *module1 = NP_TEST_MODULE_DIR "/rfc2.yang";
+    const char *module2 = NP_TEST_MODULE_DIR "/filter1.yang";
 
     /* setup environment necessary for installing module */
     NP_GLOB_SETUP_ENV_FUNC;
@@ -50,6 +51,8 @@ local_setup(void **state)
     /* connect to server and install test modules */
     assert_int_equal(sr_connect(SR_CONN_DEFAULT, &conn), SR_ERR_OK);
     assert_int_equal(sr_install_module(conn, module1, NULL, features),
+            SR_ERR_OK);
+    assert_int_equal(sr_install_module(conn, module2, NULL, features),
             SR_ERR_OK);
     assert_int_equal(sr_disconnect(conn), SR_ERR_OK);
 
@@ -65,6 +68,7 @@ local_teardown(void **state)
     /* connect to server and remove test modules */
     assert_int_equal(sr_connect(SR_CONN_DEFAULT, &conn), SR_ERR_OK);
     assert_int_equal(sr_remove_module(conn, "rfc2"), SR_ERR_OK);
+    assert_int_equal(sr_remove_module(conn, "filter1"), SR_ERR_OK);
     assert_int_equal(sr_disconnect(conn), SR_ERR_OK);
 
     /* close netopeer2 server */
@@ -184,7 +188,8 @@ test_subtree_basic(void **state)
     /* TODO: Check with multiple configs without namespace */
     struct np_test *st = *state;
     const char *RFC2_COMPLEX_DATA, *RFC2_DELETE_ALL, *RFC2_FILTER_AREA1,
-            *subtree1;
+            *subtree1, *F1_DATA, *F1_DELETE_ALL, *F1_SELECTION_NODE_TEST,
+            *F1_SELECTION_NODE_RESULT, *NO_NAMESPACE_FILTER;
 
     RFC2_COMPLEX_DATA =
             "<top xmlns=\"rfc2\">\n"                      \
@@ -229,8 +234,6 @@ test_subtree_basic(void **state)
 
     GET_CONFIG(st);
 
-    printf("%s", st->str);
-
     FREE_TEST_VARS(st);
 
     subtree1 =
@@ -273,47 +276,159 @@ test_subtree_basic(void **state)
 
     FREE_TEST_VARS(st);
 
-    /* TODO: DEBUG: Returns empty data? */
-    GET_CONFIG_FILTER(st, "");
+    /* TODO: Returns empty data on "", is that expected? */
+    /* TODO: Similar test is already in test_rpc.c but for get, is it needed? */
+    GET_CONFIG_FILTER(st, NULL);
 
     const char *RFC2_EMPTY_FILTER =
-        "<get-config xmlns=\"urn:ietf:params:xml:ns:netconf:base:1.0\">\n" \
-        "  <data>\n"                                                       \
-        "    <top xmlns=\"rfc2\">\n"                                       \
-        "      <protocols>\n"                                              \
-        "        <ospf>\n"                                                 \
-        "          <area>\n"                                               \
-        "            <name>0.0.0.0</name>\n"                               \
-        "            <interfaces>\n"                                       \
-        "              <interface>\n"                                      \
-        "                <name>192.0.2.1</name>\n"                         \
-        "              </interface>\n"                                     \
-        "              <interface>\n"                                      \
-        "                <name>192.0.2.4</name>\n"                         \
-        "              </interface>\n"                                     \
-        "            </interfaces>\n"                                      \
-        "          </area>\n"                                              \
-        "          <area>\n"                                               \
-        "            <name>192.168.0.0</name>\n"                           \
-        "            <interfaces>\n"                                       \
-        "              <interface>\n"                                      \
-        "                <name>192.168.0.1</name>\n"                       \
-        "              </interface>\n"                                     \
-        "              <interface>\n"                                      \
-        "                <name>192.168.0.12</name>\n"                      \
-        "              </interface>\n"                                     \
-        "              <interface>\n"                                      \
-        "                <name>192.168.0.25</name>\n"                      \
-        "              </interface>\n"                                     \
-        "            </interfaces>\n"                                      \
-        "          </area>\n"                                              \
-        "        </ospf>\n"                                                \
-        "      </protocols>\n"                                             \
-        "    </top>\n"                                                     \
-        "  </data>\n"                                                      \
-        "</get-config>\n";
+            "<get-config xmlns=\"urn:ietf:params:xml:ns:netconf:base:1.0\">\n" \
+            "  <data>\n"                                                       \
+            "    <top xmlns=\"rfc2\">\n"                                       \
+            "      <protocols>\n"                                              \
+            "        <ospf>\n"                                                 \
+            "          <area>\n"                                               \
+            "            <name>0.0.0.0</name>\n"                               \
+            "            <interfaces>\n"                                       \
+            "              <interface>\n"                                      \
+            "                <name>192.0.2.1</name>\n"                         \
+            "              </interface>\n"                                     \
+            "              <interface>\n"                                      \
+            "                <name>192.0.2.4</name>\n"                         \
+            "              </interface>\n"                                     \
+            "            </interfaces>\n"                                      \
+            "          </area>\n"                                              \
+            "          <area>\n"                                               \
+            "            <name>192.168.0.0</name>\n"                           \
+            "            <interfaces>\n"                                       \
+            "              <interface>\n"                                      \
+            "                <name>192.168.0.1</name>\n"                       \
+            "              </interface>\n"                                     \
+            "              <interface>\n"                                      \
+            "                <name>192.168.0.12</name>\n"                      \
+            "              </interface>\n"                                     \
+            "              <interface>\n"                                      \
+            "                <name>192.168.0.25</name>\n"                      \
+            "              </interface>\n"                                     \
+            "            </interfaces>\n"                                      \
+            "          </area>\n"                                              \
+            "        </ospf>\n"                                                \
+            "      </protocols>\n"                                             \
+            "    </top>\n"                                                     \
+            "  </data>\n"                                                      \
+            "</get-config>\n";
 
     assert_string_equal(st->str, RFC2_EMPTY_FILTER);
+
+    FREE_TEST_VARS(st);
+
+    F1_DATA =
+            "<top xmlns=\"f1\">\n"                           \
+            "  <devices>\n"                                  \
+            "    <servers>\n"                                \
+            "      <server>\n"                               \
+            "        <name>First</name>\n"                   \
+            "        <address>192.168.0.4</address>\n"       \
+            "        <port>80</port>\n"                      \
+            "      </server>\n"                              \
+            "      <server>\n"                               \
+            "        <name>Second</name>\n"                  \
+            "        <address>192.168.0.12</address>\n"      \
+            "        <port>80</port>\n"                      \
+            "      </server>\n"                              \
+            "      <server>\n"                               \
+            "        <name>Fourth</name>\n"                  \
+            "        <address>192.168.0.50</address>\n"      \
+            "        <port>22</port>\n"                      \
+            "      </server>\n"                              \
+            "      <server>\n"                               \
+            "        <name>Fifth</name>\n"                   \
+            "        <address>192.168.0.50</address>\n"      \
+            "        <port>443</port>\n"                     \
+            "      </server>\n"                              \
+            "      <server>\n"                               \
+            "        <name>Sixth</name>\n"                   \
+            "        <address>192.168.0.102</address>\n"     \
+            "        <port>22</port>\n"                      \
+            "      </server>\n"                              \
+            "    </servers>\n"                               \
+            "    <desktops>\n"                               \
+            "      <desktop>\n"                              \
+            "        <name>Seventh</name>\n"                 \
+            "        <address>192.168.0.130</address>\n"     \
+            "      </desktop>\n"                             \
+            "      <desktop>\n"                              \
+            "        <name>Sixth</name>\n"                   \
+            "        <address>192.168.0.142</address>\n"     \
+            "      </desktop>\n"                             \
+            "    </desktops>\n"                              \
+            "  </devices>\n"                                 \
+            "</top>\n";
+
+    /* Send rpc merging data into f1 */
+    SEND_EDIT_RPC(st, F1_DATA);
+
+    /* Receive a reply, should succeed */
+    ASSERT_OK_REPLY(st);
+
+    FREE_TEST_VARS(st);
+
+    F1_SELECTION_NODE_TEST =
+            "<top xmlns=\"f1\">\n"                  \
+            "  <devices>\n"                         \
+            "    <servers/>\n"                      \
+            "  </devices>\n"                        \
+            "</top>\n";
+
+    GET_CONFIG_FILTER(st, F1_SELECTION_NODE_TEST);
+
+    F1_SELECTION_NODE_RESULT =
+            "<get-config xmlns=\"urn:ietf:params:xml:ns:netconf:base:1.0\">\n" \
+            "  <data>\n"                                                       \
+            "    <top xmlns=\"f1\">\n"                                         \
+            "      <devices>\n"                                                \
+            "        <servers>\n"                                              \
+            "          <server>\n"                                             \
+            "            <name>First</name>\n"                                 \
+            "            <address>192.168.0.4</address>\n"                     \
+            "            <port>80</port>\n"                                    \
+            "          </server>\n"                                            \
+            "          <server>\n"                                             \
+            "            <name>Second</name>\n"                                \
+            "            <address>192.168.0.12</address>\n"                    \
+            "            <port>80</port>\n"                                    \
+            "          </server>\n"                                            \
+            "          <server>\n"                                             \
+            "            <name>Fourth</name>\n"                                \
+            "            <address>192.168.0.50</address>\n"                    \
+            "            <port>22</port>\n"                                    \
+            "          </server>\n"                                            \
+            "          <server>\n"                                             \
+            "            <name>Fifth</name>\n"                                 \
+            "            <address>192.168.0.50</address>\n"                    \
+            "            <port>443</port>\n"                                   \
+            "          </server>\n"                                            \
+            "          <server>\n"                                             \
+            "            <name>Sixth</name>\n"                                 \
+            "            <address>192.168.0.102</address>\n"                   \
+            "            <port>22</port>\n"                                    \
+            "          </server>\n"                                            \
+            "        </servers>\n"                                             \
+            "      </devices>\n"                                               \
+            "    </top>\n"                                                     \
+            "  </data>\n"                                                      \
+            "</get-config>\n";
+
+    assert_string_equal(st->str, F1_SELECTION_NODE_RESULT);
+
+    FREE_TEST_VARS(st);
+
+    NO_NAMESPACE_FILTER =
+            "<top/>";
+
+    GET_CONFIG_FILTER(st, NO_NAMESPACE_FILTER);
+
+    /* TODO: Change this once wildcards work */
+    printf("||%s||", st->str);
 
     FREE_TEST_VARS(st);
 
@@ -325,6 +440,20 @@ test_subtree_basic(void **state)
 
     /* Send rpc deleting part of the data from module rfc2 */
     SEND_EDIT_RPC(st, RFC2_DELETE_ALL);
+
+    /* Receive a reply, should succeed */
+    ASSERT_OK_REPLY(st);
+
+    FREE_TEST_VARS(st);
+
+    F1_DELETE_ALL =
+            "<top xmlns=\"f1\""                                         \
+            "xmlns:xc=\"urn:ietf:params:xml:ns:netconf:base:1.0\""      \
+            "xc:operation=\"delete\">"                                  \
+            "</top>";
+
+    /* Send rpc deleting part of the data from module f1 */
+    SEND_EDIT_RPC(st, F1_DELETE_ALL);
 
     /* Receive a reply, should succeed */
     ASSERT_OK_REPLY(st);
